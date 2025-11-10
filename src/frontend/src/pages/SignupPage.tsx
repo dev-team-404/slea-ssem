9d45ae3e12e11ac31e4b20d6b90d61e227ae8660
youkyoung kim <jeane2003@naver.com>
2025-11-10 21:57:05 +0900

    modify wrong description in SignUp

--- src/frontend/src/pages/SignupPage.tsx
+++ src/frontend/src/pages/SignupPage.tsx
@@ -7,7 +7,7 @@ const SignupPage: React.FC = () => {
     <main className="signup-page">
       <div className="signup-container">
         <h1 className="signup-title">회원가입</h1>
-        <p className="signup-description">환영합니다! 닉네임을 등록해주세요.</p>
+        <p className="signup-description">환영합니다! 학습 플랫폼 가입을 시작하겠습니다.</p>
         <div className="placeholder-content">
           <p>🚧 이 페이지는 REQ-F-A2 구현 대기 중입니다.</p>
           <p>닉네임 입력 및 중복 확인 기능이 추가될 예정입니다.</p>
